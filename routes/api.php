51bd8302e9815f0ab47b48436e98675190407199
apollo <vladislavvitalevich@gmail.com>
2023-07-18 12:18:38 +0300

    readme

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Api\SubscribeController;
 use App\Http\Controllers\Api\CityController;
 use App\Http\Controllers\Api\WeatherController;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group([
     'prefix' => 'auth'
 ], function () {
